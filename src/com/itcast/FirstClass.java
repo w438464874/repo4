31459fe6ed8bb513987b7a53f990ae05e771edcb
Goo <wu438464874@qq.com>
2019-12-08 21:27:58 +0800

    第一次提交firstProject

---
+++ src/com/itcast/FirstClass.java
@@ -0,0 +1,14 @@
+package com.itcast;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("helloWorld");
+        System.out.println("helloWorld");
+        System.out.println("helloWorld");
+        System.out.println("helloWorld");
+        System.out.println("helloWorld");
+        System.out.println("helloWorld");
+        System.out.println("helloWorld");
+        System.out.println("helloWorld");
+    }
+}
